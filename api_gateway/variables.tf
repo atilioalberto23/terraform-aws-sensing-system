b2fc4901dd95b25cd6ead119353fc7e538b72286
atilioalberto23 <atilio.rodriguez03@outlook.com>
2025-11-04 14:29:57 -0500

    Add files via upload

---
+++ api_gateway/variables.tf
@@ -0,0 +1,15 @@
+variable "lambda_function_arn" {
+  description = "ARN de la función Lambda existente"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "Nombre de la función Lambda existente"
+  type        = string
+}
+
+variable "lambda_invoke_arn" {
+  description = "Invoke ARN de la función Lambda"
+  type        = string
+}
+
