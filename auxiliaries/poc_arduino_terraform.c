4989e38fafbfe261be87dff86fa16fce5593a1b5
Atilio Rodriguez <atilio.rodriguez03@outlook.com>
2026-05-06 02:33:29 -0500

    ESP32 LECTURA DE SENSORES POR ORIENTACION

--- auxiliaries/poc_arduino_terraform.c
+++ auxiliaries/poc_arduino_terraform.c
@@ -12,77 +12,77 @@ const char* ssid = "CoffeeSolutionsDTX";
 const char* password = "CoffeeSolutionsDTX";
 
 // ------------------- API --------------------
-const char* serverName =  "";
+const char* serverName =
+  "https://kbmxq1670b.execute-api.us-east-1.amazonaws.com/prod/data";
 
 // ------------------- NTP --------------------
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 0;
 const int daylightOffset_sec = 0;
 
-// ------------------- PINES HUZZAH32 --------------------
-// DHT11
-#define DHTTYPE DHT11
-DHT dht1(14, DHTTYPE);   // A6
-DHT dht2(32, DHTTYPE);   // A8
-DHT dht3(12, DHTTYPE);   // A10
-DHT dht4(27, DHTTYPE);   // A12
-
-// DS18B20
-OneWire oneWire1(26);   // A0
-OneWire oneWire2(25);   // A1
-OneWire oneWire3(15);   // A3
-OneWire oneWire4(4);    // A5
-
-DallasTemperature ds1(&oneWire1);
-DallasTemperature ds2(&oneWire2);
-DallasTemperature ds3(&oneWire3);
-DallasTemperature ds4(&oneWire4);
-
-// ------------------- STRUCT DE LECTURA -------------------
-struct SensorData {
-  float temp_dht[4];
-  float hum_dht[4];
-  float temp_ds[4];
+// ------------------- SENSOR STRUCTS -------------------
+struct OrientationInfo {
+  const char* name;
+  float x;
+  float y;
 };
 
-// ------------------- HORA ISO8601 -------------------
+struct SensorPoint {
+  OrientationInfo orient;
+  uint8_t dhtPin;
+  uint8_t dsPin;
+
+  DHT dht;
+  DallasTemperature* ds;   // PUNTERO, no objeto
+};
+
+// ------------------- ORIENTACIONES -------------------
+OrientationInfo NE = {"NE",  1,  1};
+OrientationInfo NO = {"NO", -1,  1};
+OrientationInfo SE = {"SE",  1, -1};
+OrientationInfo SO = {"SO", -1, -1};
+
+// ------------------- PINES -------------------
+#define NE_DHT_PIN 32
+#define NE_DS_PIN  26
+
+#define NO_DHT_PIN 14
+#define NO_DS_PIN  15
+
+#define SE_DHT_PIN 27
+#define SE_DS_PIN  12
+
+#define SO_DHT_PIN 25
+#define SO_DS_PIN   4
+
+// ------------------- ONEWIRE + DS18B20 -------------------
+OneWire owNE(NE_DS_PIN);
+OneWire owNO(NO_DS_PIN);
+OneWire owSE(SE_DS_PIN);
+OneWire owSO(SO_DS_PIN);
+
+DallasTemperature dsNE(&owNE);
+DallasTemperature dsNO(&owNO);
+DallasTemperature dsSE(&owSE);
+DallasTemperature dsSO(&owSO);
+
+// ------------------- POINTS -------------------
+SensorPoint points[] = {
+  {NE, NE_DHT_PIN, NE_DS_PIN, DHT(NE_DHT_PIN, DHT11), &dsNE},
+  {NO, NO_DHT_PIN, NO_DS_PIN, DHT(NO_DHT_PIN, DHT11), &dsNO},
+  {SE, SE_DHT_PIN, SE_DS_PIN, DHT(SE_DHT_PIN, DHT11), &dsSE},
+  {SO, SO_DHT_PIN, SO_DS_PIN, DHT(SO_DHT_PIN, DHT11), &dsSO}
+};
+
+const int NUM_POINTS = 4;
+
+// ------------------- ISO8601 -------------------
 String getISO8601Time() {
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) return "";
   char buffer[30];
   strftime(buffer, sizeof(buffer), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
   return String(buffer);
-}
-
-// ------------------- LECTURA DE SENSORES -------------------
-SensorData readSensors() {
-  SensorData data;
-
-  // DHT11 readings
-  data.temp_dht[0] = dht1.readTemperature();
-  data.hum_dht[0]  = dht1.readHumidity();
-
-  data.temp_dht[1] = dht2.readTemperature();
-  data.hum_dht[1]  = dht2.readHumidity();
-
-  data.temp_dht[2] = dht3.readTemperature();
-  data.hum_dht[2]  = dht3.readHumidity();
-
-  data.temp_dht[3] = dht4.readTemperature();
-  data.hum_dht[3]  = dht4.readHumidity();
-
-  // DS18B20 readings
-  ds1.requestTemperatures();
-  ds2.requestTemperatures();
-  ds3.requestTemperatures();
-  ds4.requestTemperatures();
-
-  data.temp_ds[0] = ds1.getTempCByIndex(0);
-  data.temp_ds[1] = ds2.getTempCByIndex(0);
-  data.temp_ds[2] = ds3.getTempCByIndex(0);
-  data.temp_ds[3] = ds4.getTempCByIndex(0);
-
-  return data;
 }
 
 // ------------------- SETUP -------------------
@@ -96,21 +96,18 @@ void setup() {
     delay(800);
     Serial.print(".");
   }
-  Serial.println("\nConectado a WiFi!");
+  Serial.println("\nConectado!");
 
-  // Inicializar DHT
-  dht1.begin();
-  dht2.begin();
-  dht3.begin();
-  dht4.begin();
+  // Iniciar DHT y DS
+  for (int i = 0; i < NUM_POINTS; i++) {
+    points[i].dht.begin();
+  }
 
-  // Inicializar DS18B20
-  ds1.begin();
-  ds2.begin();
-  ds3.begin();
-  ds4.begin();
+  dsNE.begin();
+  dsNO.begin();
+  dsSE.begin();
+  dsSO.begin();
 
-  // Sincronizar hora NTP
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 }
 
@@ -119,64 +116,52 @@ void loop() {
 
   if (WiFi.status() == WL_CONNECTED) {
 
-    SensorData s = readSensors();
-
-    HTTPClient http;
-    http.begin(serverName);
-    http.addHeader("Content-Type", "application/json");
-
-    DynamicJsonDocument doc(1024);
+    DynamicJsonDocument doc(2048);
     doc["sensor_id"] = "sensor001";
     doc["request_time"] = getISO8601Time();
 
     JsonObject payload = doc.createNestedObject("payload");
     payload["msr_prd_id"] = 20250302115930;
 
-    // DHT11 values
-    payload["dht1_temp"] = s.temp_dht[0];
-    payload["dht1_hum"]  = s.hum_dht[0];
+    // ----- LECTURA -----
+    for (int i = 0; i < NUM_POINTS; i++) {
 
-    payload["dht2_temp"] = s.temp_dht[1];
-    payload["dht2_hum"]  = s.hum_dht[1];
+      points[i].ds->requestTemperatures();
+      float dht_temp = points[i].dht.readTemperature();
+      float dht_hum  = points[i].dht.readHumidity();
+      float ds_temp  = points[i].ds->getTempCByIndex(0);
 
-    payload["dht3_temp"] = s.temp_dht[2];
-    payload["dht3_hum"]  = s.hum_dht[2];
+      String P = points[i].orient.name;
 
-    payload["dht4_temp"] = s.temp_dht[3];
-    payload["dht4_hum"]  = s.hum_dht[3];
+      payload[(P + "_dht_temp").c_str()] = dht_temp;
+      payload[(P + "_dht_hum").c_str()]  = dht_hum;
+      payload[(P + "_ds_temp").c_str()]  = ds_temp;
 
-    // DS18B20 values
-    payload["ds1_temp"] = s.temp_ds[0];
-    payload["ds2_temp"] = s.temp_ds[1];
-    payload["ds3_temp"] = s.temp_ds[2];
-    payload["ds4_temp"] = s.temp_ds[3];
+      payload[(P + "_coord_x").c_str()] = points[i].orient.x;
+      payload[(P + "_coord_y").c_str()] = points[i].orient.y;
+    }
 
-    // Serializar JSON
+    // ----- ENVÍO -----
     String jsonData;
     serializeJson(doc, jsonData);
 
     Serial.println("\n--- JSON ENVIADO ---");
     Serial.println(jsonData);
 
-    // Enviar
-    int httpResponseCode = http.POST(jsonData);
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
 
+    int httpResponseCode = http.POST(jsonData);
     Serial.print("HTTP Response: ");
     Serial.println(httpResponseCode);
 
     if (httpResponseCode > 0) {
-      Serial.println("Respuesta del servidor:");
       Serial.println(http.getString());
-    } else {
-      Serial.print("Error enviando: ");
-      Serial.println(httpResponseCode);
     }
 
     http.end();
   }
-  else {
-    Serial.println("Error: WiFi desconectado.");
-  }
 
-  delay(10000); // 10s
+  delay(10000);
 }
